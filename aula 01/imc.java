5e1faa2c839271e83bcaf15771a808bb565e883c
Pedro Passos <pedroh.passosm7@gmail.com>
2024-03-21 07:49:38 -0300

    exercicio imc terminado

--- aula 01/imc.java
+++ aula 01/imc.java
@@ -3,27 +3,41 @@ import java.util.Scanner;
 public class imc {
     public static void main(String[] args) {
         Scanner myobj = new Scanner(System.in);
-        float peso;
-        float altura;
+        Float peso;
+        Float altura;
+        String nome;
+        String imcr;
         
 
-        
+        System.out.println("insira o seu nome:");
+        nome = myobj.nextLine();
+
         System.out.println("insira o seu peso:");
         peso = myobj.nextFloat();
 
         System.out.println("insira a sua altura:");
         altura = myobj.nextFloat();
 
-        float imc = peso/(altura * altura);
+        Float imc = peso/(altura * altura);
 
         System.out.println(imc);
 
-        if (imc < 16.9) {
-            System.out.println();
-        } else if (imc > 17 && imc <18.4) {
-            System.out.println();
+        if (imc < 17) {
+            imcr = "Muito abaixo do peso";
+        } else if (imc < 18.5) {
+            imcr = "Abaixo do peso";
+        } else if (imc < 25) {
+            imcr = "Peso normal";
+        } else if (imc < 30) {
+            imcr = "Acima do peso";
+        } else if (imc < 35) {
+            imcr = "obesidade grau I";
+        } else if (imc < 40) {
+            imcr = "obesidade grau II";
+        } else{
+            imcr = "obesidade grau III";
         }
 
-        
+        System.out.println(nome + "tem " +altura+ "m e pesa " + peso + "kg. Sua situação é: " + imcr);
     }
 }
